e153b3664a280313785c1ec2b0b52020e45c7a03
Roman Filkovsky <rom.e.f@hey.com>
2024-06-18 23:45:06 +0200

    1. Dockerfile now works 2. Added pickle import to api.py, added (for now commented out) lines for /predict 3. Added relevant libraries to requirements.txt 4. Added models directory 5. Added the pycache to .gitignore

--- project_code/api.py
+++ project_code/api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import pickle
 
 app = FastAPI()
 
@@ -8,5 +9,10 @@ def root():
 
 
 @app.get('/predict')
-def predict():
+def predict(free_text_address):
     pass
+    # with open ('../models/best_model.pkl', 'rb') as model_file:
+    #     model = pickle.load(model_file)
+    # codified_address = placeholder_function_to_codify_text(free_text_address)
+    # prediction = model.predict(codified_text) #this means we expect
+    # return prediction
