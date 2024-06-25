77c4c7a8ebc1cc4221c80a11e25d44f7fd2d3471
Marie Missalek <m2000x@gmail.com>
2024-06-25 09:38:46 +0200

    amended tidy_df function and created new shorten_df function

--- project_code/data.py
+++ project_code/data.py
@@ -34,12 +34,11 @@ def load_csv():
 
 def tidy_df(df: pd.DataFrame) -> pd.DataFrame:
     '''
-    takes london re df and deletes all columns not needed as features for model,
-    renames various columns,
-    removes nan values,
+    takes raw df and:
+    tidies some nan columns,
+    renames district and ground columns,
     removes price outliers.
     '''
-
     # replace nan values w empty strings
     df.fillna({'additional_info':''}, inplace=True)
 
@@ -47,13 +46,6 @@ def tidy_df(df: pd.DataFrame) -> pd.DataFrame:
     df['full_property_number']=df['number']+ '' + df['additional_info']
 
     df.rename(columns={'district':'borough', 'ground':'ownership'}, inplace=True)
-
-    # dropping columns
-    df.drop(columns=['day', 'number', 'street', 'additional_info',\
-        'full_property_number', 'borough', 'locality','town','county'], inplace=True)
-
-    # drop rows with NaN (3000 in postcode)
-    df.dropna(axis=0, how='any', inplace=True)
 
     # dropping rows where 'type'=='O'
     df=df[df['property_type']!='O']
@@ -66,6 +58,21 @@ def tidy_df(df: pd.DataFrame) -> pd.DataFrame:
     df=df[df['price']<15_000_000]
 
     return df
+
+
+def shorten_df(df):
+    """
+    dropping columns not needed for analysis, such as 'town', etc
+    and removes nan postcode values
+    """
+    # dropping columns
+    df.drop(columns=['day', 'number', 'street', 'additional_info',\
+        'full_property_number', 'borough', 'locality','town','county'], inplace=True)
+
+    # drop rows with NaN (3000 in postcode)
+    df.dropna(axis=0, how='any', inplace=True)
+    return df
+
 
 
 # NOT NEEDED FOR NOW as we are working with the full postcode!!
