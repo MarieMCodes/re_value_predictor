95e5e14bdf3a3be3d582b0563fa2bb0c03112529
Roman Filkovsky <rom.e.f@hey.com>
2024-06-26 23:31:33 +0200

    Frontend inputs updated + some checks run in file

--- project_code/api.py
+++ project_code/api.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
 import pickle
 
+import os
+import pandas as pd
+from keras.models import Sequential, Model
+
+
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.model_selection import KFold
+
 app = FastAPI()
 
 @app.get('/')
@@ -9,10 +17,33 @@ def root():
 
 
 @app.get('/predict')
-def predict(free_text_address):
-    pass
-    # with open ('../models/best_model.pkl', 'rb') as model_file:
-    #     model = pickle.load(model_file)
-    # codified_address = placeholder_function_to_codify_text(free_text_address)
-    # prediction = model.predict(codified_text) #this means we expect
-    # return prediction
+def predict(user_year, user_month, user_postcode, user_property_type, user_property_age, user_ground):
+    userinput = {
+        'user_year': user_year,
+        'user_month': user_month,
+        'user_postcode': user_postcode,
+        'user_property_type': user_property_type,
+        'user_property_age': user_property_age,
+        'user_groun': user_ground
+    }
+    # print(userinput)
+    # print(os.path.exists('../models/best_model.pkl'))
+    with open ('../models/best_model.pkl', 'rb') as model_file:
+        preprocessor, model = pickle.load(model_file)
+        df_userinput = pd.DataFrame(userinput)
+        transformed_userinput = preprocessor.transform(df_userinput)
+        prediction_log_return = model.predict(transformed_userinput).flatten()[0]
+        return {'prediction':prediction_log_return}
+
+# sample_data = {
+#     'year': [2022],
+#     'month': [6],
+#     'day': [21],
+#     'postcode': [N1 2JU],
+#     'property_type': ['F'],
+#     'property_age': ['N'],
+#     'ground': ['F']
+# }
+
+if __name__ == '__main__':
+    predict(user_year=1998, user_month=5, user_postcode='N1 2JU', user_property_type='F', user_property_age='O', user_ground='L')
--- project_code/frontend.py
+++ project_code/frontend.py
@@ -1,21 +1,70 @@
 import streamlit as st
+import sys
+
+
+
+import datetime as dt
 import requests
+def main(env='local'):
+    # web page title
+    st.title('London Real Estate Value Predictor')
 
-# web page title
-st.title('London Real Estate Value Predictor')
+    # we need inputs for year, month, postcode, property_type, property_age,ground
 
-# get our free text address input
-free_text_address = st.text_input(label='Enter your London address here', value='')
+    # input year
+    year = st.text_input(label='Select the year:', value='', autocomplete='202')
+    #input month
+    month = st.select_slider(label="Select the month number:",
+    options=[1,2,3,4,5,6,7,8,9,10,11,12])
+    #input postcode
+    postcode = st.text_input(label='Enter your postcode:', value='')
+    #input property_type
+    property_type = st.radio(label='Choose property type: F, T, S, D or O ', options=['F', 'T', 'S', 'D' ,'O'])
+    #input property_age
+    property_age = st.radio(label='Choose property age: O-Old, N-New', options=['O','N'])
+    #input ground
+    ground = st.radio(label='Ground', options=['L','F'])
 
-# Method 1 - local
-url = 'http://127.0.0.1:8000/'
 
-# Method 2 - on streamlit
-# url = 'https://basic-api-32vj2qxrpq-ew.a.run.app' #here goes the google run container instance API URL
+    if year and month and postcode and property_type and property_age and ground:
+        params = {
+            'user_year': int(year),
+            'user_month': int(month),
+            'user_postcode': str(postcode),
+            'user_property_type': str(property_type),
+            'user_property_age': str(property_age),
+            'user_ground': str(ground),
+        }
+        input_complete = True
+        # Method 1 - local
+        local_url_address_base = 'http://127.0.0.1:8000'
+
+        # Method 2 - on streamlit
+        online_url_address_base = 'https://basic-api-32vj2qxrpq-ew.a.run.app/predict' #here goes the google run container instance API URL
+
+        # check if we run locally or on the cloud
+        if env == 'local':
+            url = f'{local_url_address_base}/predict'
+        else:
+            url = f'{online_url_address_base}/predict'
 
 
-response = requests.get(url=url).json()
+    try:
+        if st.button("Model Prediction") and input_complete:
+            # st.write(f'{url}/{params}')
+            response = requests.get(url=url, params=params).json()
+            st.write('The answer is', str(response))
+    except UnboundLocalError:
+        st.write('No input or input incomplete.')
 
+if __name__ == '__main__':
+    main(env='local')
+else:
+    main(env='nonlocal')
 
-
-st.write('The answer is', str(response))
+# to be done:
+# 1. find out how we can know if we run locally, then use that to alter the base_url part of the url above
+# 2. make the frontend+api files work locally
+# 3. build the local docker and check that it works locally with api + streamlit run from cli
+# 4. build the online docker and check that it works online with api + streamlit run from cli
+# 5. build the online docker and check that it works with online streamlit app
